8f69b52ee8c0a0aa5c5926440629ff85b08bce8c
Devesh Paryani <127661847+Devesh102030@users.noreply.github.com>
2026-02-19 09:28:58 +0530

    Enhance Signup form with email and password validation
    Updated the Signup component to include an email field and improved password validation. Adjusted button styles and disabled state based on password length.

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -45,30 +45,43 @@ const Signup = () => {
         </Link>
       </nav>
 
-      <div className="bg-gradient-to-br from-gray-800 to-gray-900 p-10 rounded-3xl shadow-2xl w-full max-w-md text-center mt-20 border border-gray-700 transition-all duration-500 hover:shadow-purple-700/30 w-full max-w-md ">
+      <div className="bg-gradient-to-br from-gray-800 to-gray-900 p-10 rounded-3xl shadow-2xl  text-center mt-20 border border-gray-700 transition-all duration-500 hover:shadow-purple-700/30 w-full max-w-md ">
        
         <div className="text-center mb-6">
           <Heading label="Sign Up" />
         </div>
-      
-        <div className="flex flex-col items-center space-y-4">
-          <Text label="First Name" change={(e) => setFirstname(e.target.value)} />
-          <Text label="Last Name" change={(e) => setLastname(e.target.value)} />
-          <Text label="Username" change={(e) => setUsername(e.target.value)} />
-          <Password change={(e) => setPassword(e.target.value)} />
-        </div>
+      <div className="flex flex-col items-center space-y-5">
+  <Text label="First Name" change={(e) => setFirstname(e.target.value)} />
+  <Text label="Last Name" change={(e) => setLastname(e.target.value)} />
+  <Text label="Email" change={(e) => setUsername(e.target.value)} />
+
+  <Password
+    placeholder="min 8 char"
+    change={(e) => setPassword(e.target.value)}
+  />
+
+  {password.length > 0 && password.length < 8 && (
+    <p className="text-red-500 text-sm ">
+      Password must be at least 8 characters
+    </p>
+  )}
+
+
         
-        <div className="mt-8">
-          <button
-            onClick={handleSignup}
-            className="w-40 bg-gray-700 hover:bg-gray-600  
-            hover:text-white text-lg font-semibold py-3 rounded-xl 
-            shadow-md hover:shadow-cyan-500/20 transition-all 
-            duration-300 ease-in-out transform hover:scale-105 border border-gray-600"
-          >
-            Sign Up
-          </button>
-        </div>
+        <button
+  onClick={handleSignup}
+  disabled={password.length < 8}
+  className={`w-35 text-lg font-semibold py-2.5 rounded-xl transition-all
+    ${
+      password.length < 8
+        ? "bg-gray-600 cursor-not-allowed"
+        : "bg-gray-700 hover:bg-gray-600"
+    }`}
+>
+  Sign Up
+</button>
+</div>
+
 
         <div className="text-center mt-4">
           <p className="text-gray-300">
@@ -84,5 +97,3 @@ const Signup = () => {
 };
 
 export default Signup;
-
-
